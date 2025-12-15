9f540fa09aa3c3bcbfcfb596ba6d7076011f49e7
Utkarsh Rawat <utkarshrawat@Utkarshs-MacBook-Air.local>
2025-12-14 20:03:30 -0800

    Add Google Analytics component

---
+++ components/GoogleAnalytics.tsx
@@ -0,0 +1,28 @@
+import Script from "next/script";
+
+type GoogleAnalyticsProps = {
+  measurementId: string;
+};
+
+export default function GoogleAnalytics({
+  measurementId,
+}: GoogleAnalyticsProps) {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
+        strategy="afterInteractive"
+      />
+      <Script id="ga-gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${measurementId}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+}
